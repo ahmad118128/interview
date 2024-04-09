79713e6a3b3b70c45b18ef386c86ce72f8a0a83a
SomayeShafiei76 <somayehShafieiDeveloper@example.com>
2024-04-09 15:13:33 +0330

    install react-hook-form and change type of textfield rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { TextField } from '@/components/atoms/TextField';
+import { useForm } from 'react-hook-form';
+
 export default function Home() {
+  const { control } = useForm();
   return (
     <>
-      <p>
-      main page
-      </p>
+      <p>main page</p>
+      <TextField name="aaa" control={control} />
     </>
   );
 }

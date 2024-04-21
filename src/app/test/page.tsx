5d771e6bc1bb3ad19d14f6d39e64dab91f95b753
SomayeShafiei76 <somayehShafieiDeveloper@example.com>
2024-04-21 16:21:44 +0330

    refactor:build baseInput without style#2

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { BaseInput } from '@/baseComponents/atoms/Input';
+import { FieldValues, useForm } from 'react-hook-form';
+
+export default function Test() {
+  const { control } = useForm<FieldValues>();
+  return <BaseInput name={'test'} control={control} variant='filled'/>;
+}
---
+++ src/baseComponents/atoms/Input/type.ts
@@ -0,0 +1,47 @@
+import {
+  InputProps,
+  InputBaseComponentProps,
+  TextFieldVariants,
+} from '@mui/material';
+import { HTMLInputTypeAttribute, ReactElement, ChangeEvent } from 'react';
+import {
+  FieldValues,
+  UseControllerProps,
+  UseFormSetError,
+  ValidationValueMessage,
+} from 'react-hook-form';
+
+export type StyledTextFieldRtlType = {
+  ltrValue?: boolean;
+  ltrLabel?: boolean;
+  typeInput?: HTMLInputTypeAttribute;
+};
+export type ValidateType =
+  | 'en'
+  | 'fa'
+  | 'num'
+  | 'money'
+  | 'cardNumber'
+  | 'fa_spc'
+  | 'enCharacters';
+export interface BaseInputProps extends UseControllerProps {
+  element?: ReactElement;
+  type?: HTMLInputTypeAttribute;
+  endAdornment?: ReactElement;
+  startAdornment?: ReactElement;
+  ltrValue?: 'ltr' | 'rtl';
+  ltrLabel?: boolean;
+  label?: string;
+  validateType?: ValidateType;
+  inputBaseProps?: InputBaseComponentProps;
+  inputProps?: InputProps;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  id?: string;
+  className?: string;
+  setError?: UseFormSetError<FieldValues>;
+  variant?: TextFieldVariants;
+  changeMiddleWare?: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+}

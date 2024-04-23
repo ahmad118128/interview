020ef6acf7e99aa83b4a478984bc66d8514eeb48
Mohammad Ehsan <m.ehsan1381@gmail.com>
2024-04-23 11:10:50 +0330

    use theme in font#13

--- src/components/atoms/CustomTooltip/index.tsx
+++ src/components/atoms/CustomTooltip/index.tsx
@@ -1,19 +1,20 @@
 'use client';
 import { styled } from '@mui/system';
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import theme from '@/theme';
 
 export const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
 ))`
   & .${tooltipClasses.tooltip} {
-    background-color: ${({ theme }) => theme.palette.primary.main};
+    background-color: ${theme.palette.primary.main};
     color: '#fff';
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 20px;
+    font-size: ${theme.typography.body2.fontSize};
+    font-weight: ${theme.typography.body2.fontWeight};
+    line-height: ${theme.typography.body2.lineHeight};
     padding: 0.375rem 0.5rem;
   }
   & .${tooltipClasses.arrow} {
-    color: ${({ theme }) => theme.palette.primary.main};
+    color: ${theme.palette.primary.main};
   }
 `;

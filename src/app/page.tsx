e2b76d981db816b41f101dd188a76bfc5742aa6e
Mohammad Ehsan <m.ehsan1381@gmail.com>
2024-04-24 09:01:35 +0330

    use tab in page#15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { CustomTab } from '@/components/molecules/CustomTab/styled';
 import { Typography } from '@mui/material';
+import { Icon } from '@iconify/react';
 
 export default function Home() {
   return (
@@ -11,6 +15,56 @@ export default function Home() {
       <Typography variant="subtitle1">subtitle1</Typography>
       <Typography variant="subtitle2">subtitle2</Typography>
       <Typography variant="button">button</Typography>
+      <CustomTab
+        type="buttonTab"
+        data={[
+          {
+            id: 0,
+            label: (
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '7px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Icon icon="ion:filter" />
+                تست1
+              </div>
+            ),
+            tabPanel: <div>test1</div>,
+          },
+          { id: 1, label: 'تست2', tabPanel: <div>test2</div>, disabled: true },
+          { id: 2, label: 'تست3', tabPanel: <div>test3</div> },
+          { id: 3, label: 'تست4', tabPanel: <div>test4</div> },
+        ]}
+      />
+      <CustomTab
+        type="normalTab"
+        data={[
+          {
+            id: 0,
+            label: (
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '7px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Icon icon="ion:filter" />
+                تست1
+              </div>
+            ),
+            tabPanel: <div>test1</div>,
+          },
+          { id: 1, label: 'تست2', tabPanel: <div>test2</div> },
+          { id: 2, label: 'تست3', tabPanel: <div>test3</div> },
+          { id: 3, label: 'تست4', tabPanel: <div>test4</div> },
+        ]}
+      />
     </>
   );
 }

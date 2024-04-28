b688689ad402fd016a4e91ac55b51b09b3e92493
ehsan <m.ehsan1381@gmail.com>
2024-04-28 15:26:15 +0330

    use custom tab in page#15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,39 @@ export default function Home() {
       <Typography variant="body2">تست</Typography>
       <Typography variant="caption">تست</Typography>
       <Typography variant="button">تست</Typography>
+      <CustomTab
+        type="buttonTab"
+        data={[
+          {
+            id: 0,
+            label: <div>تست1</div>,
+            icon: 'gravity-ui:magnifier',
+            tabPanel: <div>test1</div>,
+          },
+          { id: 1, label: 'تست2', tabPanel: <div>test2</div>, disabled: true },
+          {
+            id: 2,
+            label: 'تست3',
+            tabPanel: <div>test3</div>,
+            icon: 'ion:filter',
+          },
+          { id: 3, label: 'تست4', tabPanel: <div>test4</div> },
+        ]}
+      />
+      <CustomTab
+        type="normalTab"
+        data={[
+          {
+            id: 0,
+            label: <div>تست1</div>,
+            icon: 'ion:filter',
+            tabPanel: <div>test1</div>,
+          },
+          { id: 1, label: 'تست2', tabPanel: <div>test2</div>, disabled: true },
+          { id: 2, label: 'تست3', tabPanel: <div>test3</div> },
+          { id: 3, label: 'تست4', tabPanel: <div>test4</div> },
+        ]}
+      />
     </>
   );
 }

52a662fb1c68aadaec5702507e957c7d4f56ea51
SomayeShafiei76 <somayehShafieiDeveloper@example.com>
2024-05-02 13:59:25 +0330

    refactor:change fonts

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,9 +7,7 @@ import {
   StyledMainLayout,
 } from './styled';
 
-export default function AdminLayout(props: PropsWithChildren) {
-  const { children } = props;
-
+export default function AdminLayout({ children }: PropsWithChildren) {
   return (
     <>
       <StyledContainerLayout>

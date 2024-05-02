163862bc9a18b9a3260527854ade512af4574839
SomayeShafiei76 <somayehShafieiDeveloper@example.com>
2024-05-02 11:46:49 +0330

    refactor:delete test divs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,29 +2,5 @@
 import { Box } from '@mui/material';
 
 export default function Dashboard() {
-  return (
-    <div
-      style={{
-        background: 'yellow',
-        padding: '7px',
-        width: '100%',
-        border: '2px solid #ff0',
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '20px',
-        flexWrap: 'nowrap',
-      }}
-    >
-      <p>test components page</p>
-      <Box
-        sx={{ width: '700px', height: '800px', backgroundColor: 'grey' }}
-      ></Box>
-      <Box
-        sx={{ width: '700px', height: '800px', backgroundColor: 'grey' }}
-      ></Box>
-      <Box
-        sx={{ width: '700px', height: '800px', backgroundColor: 'grey' }}
-      ></Box>
-    </div>
-  );
+  return <>dashboard page</>;
 }

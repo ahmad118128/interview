ea7a0573790f56d5b7862a69880071c9cc8e45bc
SomayeShafiei76 <somayehShafieiDeveloper@example.com>
2024-05-16 16:26:37 +0330

    fix:wrap form and add buttons of Form

--- src/app/dashboard/imageRecognition/page.tsx
+++ src/app/dashboard/imageRecognition/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import {
+  Box,
   FormControl,
   FormControlLabel,
   Radio,
@@ -11,89 +12,120 @@ import { useForm } from 'react-hook-form';
 import { CustomInput } from '@/components/atoms/input/controlledCustomInput';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import theme from '@/theme';
+import { CustomButton } from '@/components/atoms/CustomButton';
 
 export default function ImageRecognition() {
-  const { control } = useForm();
   const [selected, setSelected] = useState('female');
+
+  const {
+    setValue,
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <StyledContainerImageRecognition>
-      <FormControl sx={{ width: '100%', paddingY: 0 }}>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="female"
-          name="radio-buttons-group"
-          value={selected}
-          onChange={(ev) => {
-            console.log(ev.target.value);
-            setSelected(ev.target.value);
-          }}
-          sx={{ width: '100%', paddingY: 0 }}
-        >
-          <FormControlLabel
-            value="female"
-            control={<Radio />}
-            label="Female"
-            className="uploadIcon"
-            sx={{
-              marginRight: 0,
-              marginBottom: selected === 'female' ? '2rem' : 0,
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl sx={{ width: '100%', paddingY: 0 }}>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            value={selected}
+            onChange={(ev) => {
+              console.log(ev.target.value);
+              setSelected(ev.target.value);
             }}
-          />
-
-          {selected === 'female' && (
-            <CustomInput
-              control={control}
-              name="InputFile"
-              fullWidth
-              sx={{ marginBottom: '2rem' }}
-              label="بارگذاری تصویر"
-              type="file"
-              InputProps={{
-                endAdornment: (
-                  <Icon
-                    icon="material-symbols:upload-rounded"
-                    width="20"
-                    height="20"
-                    className="uploadIcon"
-                  />
-                ),
+            sx={{ width: '100%', paddingY: 0 }}
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+              className="uploadIcon"
+              sx={{
+                marginRight: 0,
+                marginBottom: selected === 'female' ? '2rem' : 0,
               }}
             />
-          )}
 
-          <FormControlLabel
-            value="male"
-            control={<Radio />}
-            label="male"
-            sx={{
-              marginRight: 0,
-              marginTop: selected === 'female' ? '2rem' : 0,
-              marginBottom: selected === 'male' ? '2rem' : 0,
-            }}
-          />
-          {selected === 'male' && (
-            <CustomInput
-              control={control}
-              name="InputFile"
-              fullWidth
-              type="file"
-              label="بارگذاری تصویر"
-              sx={{ marginBottom: '2rem' }}
-              InputProps={{
-                endAdornment: (
-                  <Icon
-                    icon="material-symbols:upload-rounded"
-                    height="20"
-                    width="20"
-                    className="uploadIcon"
-                  />
-                ),
+            {selected === 'female' && (
+              <CustomInput
+                control={control}
+                name="InputFile"
+                fullWidth
+                sx={{ marginBottom: '2rem' }}
+                label="بارگذاری تصویر"
+                type="file"
+                InputProps={{
+                  endAdornment: (
+                    <Icon
+                      icon="material-symbols:upload-rounded"
+                      width="20"
+                      height="20"
+                      className="uploadIcon"
+                    />
+                  ),
+                }}
+              />
+            )}
+
+            <FormControlLabel
+              value="male"
+              control={<Radio />}
+              label="male"
+              sx={{
+                marginRight: 0,
+                marginTop: selected === 'female' ? '2rem' : 0,
+                marginBottom: selected === 'male' ? '2rem' : 0,
               }}
             />
-          )}
-        </RadioGroup>
-      </FormControl>
+            {selected === 'male' && (
+              <CustomInput
+                control={control}
+                name="InputFile"
+                fullWidth
+                type="file"
+                label="بارگذاری تصویر"
+                sx={{ marginBottom: '2rem' }}
+                InputProps={{
+                  endAdornment: (
+                    <Icon
+                      icon="material-symbols:upload-rounded"
+                      height="20"
+                      width="20"
+                      className="uploadIcon"
+                    />
+                  ),
+                }}
+              />
+            )}
+          </RadioGroup>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              gap: '0.75rem',
+              marginTop: '2rem',
+            }}
+          >
+            <CustomButton variant="contained" type="submit">
+              شناسایی مجدد
+            </CustomButton>
+            <CustomButton
+              variant="outlined"
+              onClick={() => setValue('InputFile', '')}
+              disabled={!watch('InputFile')}
+            >
+              انصراف
+            </CustomButton>
+          </Box>
+        </FormControl>
+      </form>
     </StyledContainerImageRecognition>
   );
 }

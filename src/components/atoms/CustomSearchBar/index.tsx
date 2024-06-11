d5151fa53fae625661382b3cd692911bb0140de6
saghi <saghi7699@gmail.com>
2024-06-11 11:05:27 +0330

    feat storyCustomSearchBar storybook #50

--- src/components/atoms/CustomSearchBar/index.tsx
+++ src/components/atoms/CustomSearchBar/index.tsx
@@ -1,4 +1,5 @@
-import { searchbarProps } from '../../baseComponents/BaseSearchBar/type';
+// import { searchbarProps } from '../../baseComponents/BaseSearchBar/type';
+import { searchbarProps } from '@/components/BaseComponents/BaseSearchBar/type';
 import { StyledSearchBar } from './styled';
 
 export default function CustomSearchBar(
---
+++ src/stories/customSearchBar.stories.tsx
@@ -0,0 +1,23 @@
+import CustomSearchBar from '@/components/atoms/CustomSearchBar';
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/theme';
+
+const meta: Meta<typeof CustomSearchBar> = {
+  title: 'atoms/CustomSearchBar',
+  component: CustomSearchBar,
+  tags: ['autodocs'],
+
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Base: Story = {
+  render: (args) => <CustomSearchBar {...args} />,
+};

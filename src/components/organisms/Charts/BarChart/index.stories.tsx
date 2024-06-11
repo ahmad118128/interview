dfdf64eb5065dca0cc660e0587724025c37c977d
AmirRoobin <amirtoolabi81@gmail.com>
2024-06-11 17:43:35 +0330

    feat: use ThemeProvider in storybook

--- src/components/organisms/Charts/BarChart/index.stories.tsx
+++ src/components/organisms/Charts/BarChart/index.stories.tsx
@@ -2,10 +2,13 @@ import * as React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { BarChart } from '.';
 import { BarChartProps } from './types';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/theme';
 
 export default {
   title: 'BarChart',
   component: BarChart,
+  decorators:[(Story)=> (<ThemeProvider theme={theme}><Story /></ThemeProvider>)]
 } as Meta;
 
 const Template: StoryFn<BarChartProps> = (args) => {

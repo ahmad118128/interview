0f55740c16d7ac3560e9d82a1f6428469eab6f79
saghi <saghi7699@gmail.com>
2024-06-13 14:31:40 +0330

    refactor : customSearchBar storybook #50

--- src/components/atoms/CustomSearchBar/customSearchBar.stories.tsx
+++ src/components/atoms/CustomSearchBar/customSearchBar.stories.tsx
@@ -19,9 +19,48 @@ const meta: Meta<typeof CustomSearchBar> = {
     name: '',
     placeholder: '',
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This is the default state of the `CustomSearchBar` component.',
+      },
+    },
+  },
+  argTypes: {
+    searchHandler: {
+      control: () => {},
+      description: 'on click button for search bar.',
+    },
+    name: { control: 'text', description: 'Name attribute for search bar.' },
+    placeholder: {
+      control: 'text',
+      description: 'place holder for search bar.',
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
 export const Base: Story = {
   render: (args) => <CustomSearchBar {...args} />,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This is the default state of the `CustomSearchBar` component.',
+      },
+    },
+  },
+};
+export const onClick: Story = {
+  args: { searchHandler: () => {} },
+  render: (args) => <CustomSearchBar {...args} />,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This is the onClick state of the `CustomSearchBar` component.',
+      },
+    },
+  },
 };

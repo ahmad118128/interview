ec138ffa3b90559fd8861baa4c1726e34fd0662a
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-06-20 13:16:56 +0330

    style: change row data style #88

--- src/app/dashboard/data-bank/[...slug]/page.tsx
+++ src/app/dashboard/data-bank/[...slug]/page.tsx
@@ -3,9 +3,6 @@ import { DynamicRouteProps } from '../type';
 import AddForm from '@/components/pages/dashboard/data-bank/usersList/AddForm';
 import AddFormGroup from '@/components/pages/dashboard/data-bank/groupList/AddFormGroup';
 import EditForm from '@/components/pages/dashboard/data-bank/usersList/EditForm';
-import EditFormGroup from '@/components/pages/dashboard/data-bank/groupList/EditFormGroup';
-import AddGroupOfUser from '@/components/pages/dashboard/data-bank/groupRegister/AddGroupOfUser';
-import EditGroupOfUser from '@/components/pages/dashboard/data-bank/groupRegister/EditGroupOfUser';
 
 export default function Page({ params }: { params: DynamicRouteProps }) {
   const path = params.slug[0];
@@ -16,8 +13,6 @@ export default function Page({ params }: { params: DynamicRouteProps }) {
   return (
     (path === 'addUser' && <AddForm />) ||
     (path === 'editUser' && <EditForm />) ||
-    (path === 'addGroup' && <AddFormGroup />) ||
-    (path === 'addgroupofuser' && <AddGroupOfUser />) ||
-    (path === 'editgroupofuser' && <EditGroupOfUser />)
+    (path === 'addGroup' && <AddFormGroup />)
   );
 }

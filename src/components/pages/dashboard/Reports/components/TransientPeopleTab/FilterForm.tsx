1b33fa65b1d860ede6b266eba3009fcd64e4c5ca
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-06-22 00:33:30 +0330

    style: fix responsive bug #88

--- src/components/pages/dashboard/Reports/components/TransientPeopleTab/FilterForm.tsx
+++ src/components/pages/dashboard/Reports/components/TransientPeopleTab/FilterForm.tsx
@@ -5,10 +5,11 @@ import { CustomRHFAutocomplete } from '@/components/atoms/Autocomplete';
 import { CustomButton } from '@/components/atoms/CustomButton';
 
 import ControlledTimeDatePickerInput from '@/components/organisms/TimeDatePicker/ControlledTimeDatePicker';
+import { StyledFilterChild } from '../../../image-recognition/FilterChild/styled';
 
 export default function FilterForm({ control, reset }: any) {
   return (
-    <Grid container spacing={{ xs: 8, md: 8 }}>
+    <StyledFilterChild container spacing={{ xs: 8, md: 8 }}>
       <Grid item xs={12} md={4}>
         <CustomRHFAutocomplete
           label={ReportRoute.gateName}
@@ -108,6 +109,6 @@ export default function FilterForm({ control, reset }: any) {
           {DataBankRoute.earase}
         </CustomButton>
       </Box>
-    </Grid>
+    </StyledFilterChild>
   );
 }

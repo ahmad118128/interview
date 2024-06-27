da40fe6f1491206386c74f9bfa59fd8f5b67fb87
saghi <saghi7699@gmail.com>
2024-06-27 14:54:00 +0330

    refactor : radiobutton #52

--- src/components/atoms/CustomRadioButton/customRadioButton.stories.tsx
+++ src/components/atoms/CustomRadioButton/customRadioButton.stories.tsx
@@ -1,15 +1,9 @@
 import { CustomRadioButton } from '@/components/atoms/CustomRadioButton';
-import {
-  ICustomRadio,
-  IStyledRadio,
-} from '@/components/atoms/CustomRadioButton/type';
-import { Controller, useForm } from 'react-hook-form';
-
+import { ICustomRadio } from '@/components/atoms/CustomRadioButton/type';
 import theme from '@/theme';
 import { ThemeProvider } from '@emotion/react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Control, FieldError, RegisterOptions } from 'react-hook-form';
-import { types } from 'util';
+import { Control, RegisterOptions } from 'react-hook-form';
 import { StyledRadioButton } from './styled';
 const meta: Meta<typeof CustomRadioButton> = {
   title: 'atoms/CustomRadioButton',
@@ -31,73 +25,9 @@ const meta: Meta<typeof CustomRadioButton> = {
     rules: {} as RegisterOptions,
     control: {} as Control,
   },
-  argTypes: {},
 };
 export default meta;
 type Story = StoryObj<ICustomRadio>;
-
-export const Base: Story = {
-  render: (args) => <CustomRadioButton {...args} />,
-};
-export const Off: Story = {
-  parameters: {
-    docs: {
-      description: {
-        story: 'radio button in off state.',
-      },
-    },
-  },
-  render: (args) => <CustomRadioButton {...args} />,
-};
-export const On: Story = {
-  parameters: {
-    docs: {
-      description: {
-        story: 'radio button in on state.',
-      },
-    },
-  },
-  render: (args) => <CustomRadioButton {...args} />,
-};
-
-export const Error: Story = {
-  parameters: {
-    docs: {
-      description: {
-        story: 'radio button in error state.',
-      },
-    },
-  },
-  render: (args) => <CustomRadioButton {...args} />,
-};
-export const Disabled: Story = {
-  parameters: {
-    docs: {
-      description: {
-        story: 'radio button in disabled state.',
-      },
-    },
-  },
-
-  render: (args) => <CustomRadioButton {...args} />,
-};
 export const RadioItems: Omit<Story, 'control'> = {
-  args: {
-    name: '',
-  },
-
-  render: ({ name, value }) => (
-    <>
-      <StyledRadioButton
-        value={name}
-        key={value}
-        name={name}
-        disabled
-        // error
-        checked
-      />
-    </>
-  ),
-  //   <CustomRadioButton
-  //  {...args} />
+  render: (args) => <StyledRadioButton {...args} />,
 };

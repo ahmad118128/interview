797c695af87cce4afa93ae5e426ece53621f2c97
e.rakhshani <erfanrakhsh83@gmail.com>
2024-07-18 16:52:23 +0330

    Refactor: AddUser Responsive #100

--- src/components/pages/dashboard/UsersManagement/Forms/styled.ts
+++ src/components/pages/dashboard/UsersManagement/Forms/styled.ts
@@ -2,20 +2,6 @@ import theme from '@/theme';
 import { Box, Grid, styled } from '@mui/material';
 import { ControledCheckbox } from '@/components/atoms/Checkbox';
 
-// export const StyledBox = styled(Box)`
-// display: flex;
-// align-items: center;
-// padding: 0 3.75rem;
-// height: 3rem;
-// border: 1px solid ${theme.palette.grey[200]};
-// background: ${theme.palette.grey[50]};
-// border-radius: 0.625rem;
-//   @media (max-width: 775px) {
-//     height: 116px;
-//     flex-direction: column;
-//   }
-// `;
-
 export const StyledBox = styled(Box)(({ theme }) => ({
   display: 'flex',
   padding: '0 3.75rem',

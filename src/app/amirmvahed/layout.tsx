094f4cfc9d117bd4e52e37c6b8415a4f4b957159
amirmvahed <amirmvahed@gmail.com>
2024-08-08 03:03:48 -0400

    feat: Added layout to show sidebar and footer

---
+++ src/app/amirmvahed/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Grid } from '@mui/material';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Grid container spacing={2} sx={{ height: '100vh', direction: 'ltr' }}>
+      <Grid item xs={2} sx={{ backgroundColor: '#f0f0f0' }}>
+        <Box sx={{ padding: 2, height: '100%' }}>Sidebar</Box>
+      </Grid>
+      <Grid
+        item
+        xs={10}
+        sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+      >
+        <Box sx={{ flexGrow: 1, padding: 2 }}>{children}</Box>
+        <Box sx={{ backgroundColor: '#f0f0f0', padding: 2 }}>Footer</Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Layout;
